004ecbe7197828e0f5e368a8aa5f695eb3231f95
Purp1e <438518244@qq.com>
2026-01-29 17:15:07 +0800

    fix build error with cache

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,15 @@
 import { H2 } from '../ui/Heading'
 import type { Metadata } from 'next'
-
-// 静态页面，完全静态生成，缓存1天
-export const revalidate = 86400
+import { cacheLife } from 'next/cache'
 
 export const metadata: Metadata = {
   title: '关于',
   description: 'HLAE中文站 —— CS视频制作相关导航与资源集合的中文门户网站',
 }
 
-export default function About() {
+export default async function About() {
+  'use cache'
+  cacheLife('days') // 静态页面，缓存 1 天
   return (
     <div className="flex flex-col gap-2 text-zinc-900 dark:text-zinc-100 items-center justify-start min-h-screen max-w-screen-lg mx-auto py-32 px-8">
       <H2 className="mb-4">关于</H2>
